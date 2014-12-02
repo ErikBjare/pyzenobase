f30bc5936de8bf53539ec3142c4b050d6db63bf2
Eric Jain <ejain@zenobase.com>
2014-12-02 00:06:33 -0800

    Make unit tests more assertive

--- pyzenobase/tests/tests.py
+++ pyzenobase/tests/tests.py
@@ -5,37 +5,33 @@ import json
 from pprint import pprint
 import unittest
 from random import randint
-
 import requests
+import time
 
 from pyzenobase import *
 
 class ZenobaseTests(unittest.TestCase):
     def setUp(self):
         self.zapi = ZenobaseAPI()
+        self.assertEqual(self.zapi.list_buckets()["total"], 0)
         bucket = self.zapi.create_or_get_bucket("Test - PyZenobaseAPI", description="This bucket is used by PyZenobaseAPI for testing.")
+        time.sleep(1)
+        self.assertEqual(self.zapi.list_buckets()["total"], 1)
         self.bucket_id = bucket["@id"]
-
-    def tearDown(self):
-        pass
-
-    def _test_bucket(self):
-        buckets = self.zapi.list_buckets()
-        n_buckets = buckets["total"]
-        data = self.zapi.create_bucket("Test bucket", description="This bucket is for testing.")
-        self.assertTrue(n_buckets+1 == self.zapi.list_buckets()["total"])
-        self.zapi.delete_bucket(data["@id"])
-        self.assertTrue(n_buckets == self.zapi.list_buckets()["total"])
     
-    def test_bucket(self):
-        data = self.zapi.create_bucket("Test bucket", description="This bucket is for testing.")
-        self.zapi.delete_bucket(data["@id"])
-
     def test_event(self):
+        events = self.zapi.list_events(self.bucket_id)["events"]
+        self.assertEqual(len(events), 0)
         event = ZenobaseEvent({"timestamp": "2014-09-06T19:35:00.000+02:00", "rating": randint(0, 100)})
         self.zapi.create_event(self.bucket_id, event)
-        pprint(self.zapi.list_events(self.bucket_id))
-        
+        time.sleep(1)
+        events = self.zapi.list_events(self.bucket_id)["events"]
+        self.assertEqual(len(events), 1)
+        self.assertEqual(events[0]["timestamp"], event["timestamp"])
+        self.assertEqual(events[0]["rating"], event["rating"])
+
+    def tearDown(self):
+        self.zapi.delete_bucket(self.bucket_id)
 
 if __name__ == "__main__":
     unittest.main()
